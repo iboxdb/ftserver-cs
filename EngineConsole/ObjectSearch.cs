8cc7cef1b460c7a75a08ca5aca4363d9fb4f43e7
unknown-user <unknown-user@192.168.117.146>
2017-02-24 08:17:32 -0800

    text object example

---
+++ EngineConsole/ObjectSearch.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using iBoxDB.LocalServer;
+
+namespace FTServer
+{
+	public class ObjectSearch
+	{
+		public static void test_main ()
+		{
+			DB.Root ("/tmp/");
+			iBoxDB.DBDebug.DDebug.DeleteDBFiles (7);
+			DB db = new DB (7);
+
+			db.GetConfig ().DBConfig.EnsureTable<TextObject> ("TextObject", "ID");
+
+			Engine engine = new Engine ();
+			engine.Config (db.GetConfig ().DBConfig);
+
+			AutoBox auto = db.Open ();
+
+			{
+				TextObject to = new TextObject ();
+				to.ID = 1;
+				to.Season = 2018;
+				to.Group = 3;
+				to.Time = new DateTime (2018, 3, 5);
+				to.Keys = new string[] { "COOL", "FAST" };
+				to.KVDesc = new Dictionary<string, object> { { "Name", "X-MAN" }, { "Speed", 3000 } };
+				auto.Insert<TextObject> ("TextObject", to);
+				engine.indexTextNoTran (auto, int.MaxValue, to.ID, to.ToString (), false);
+
+				TextObject to2 = new TextObject ();
+				to2.ID = 2;
+				to2.Season = 2018;
+				to2.Group = 4;
+				to2.Time = new DateTime (2018, 3, 6);
+				to2.Keys = new string[] { "Sharp" };
+				to2.KVDesc = new Dictionary<string, object> { { "Speed", "gt4000" } };
+				auto.Insert<TextObject> ("TextObject", to2);
+				engine.indexTextNoTran (auto, int.MaxValue, to2.ID, to2.ToString (), false);
+			}
+			//SQL and
+			//auto.Select ("from TextObject where Season==?");
+			//auto.Select ("from TextObject where Group==?");
+			//auto.Select ("from TextObject where Season==? & Group==?");
+			//auto.Select ("from TextObject where Time==?");
+			//auto.Select ("from TextObject where Season==? & Time==?");
+
+			//Full text search -and
+			using (var box = auto.Cube()) {
+				Console.WriteLine ("Search: Season=2018");
+				String searchText = "SE-" + 2018;
+				foreach (var kw in	engine.searchDistinct(box, searchText , long.MaxValue, 200)) {
+					Console.WriteLine (box ["TextObject", kw.getID ()].Select<TextObject> ());
+				}
+
+				Console.WriteLine ("\r\nSearch: KVDesc={ \"Name\", \"X-MAN\" } , Keys={COOL}");
+				searchText = "KV-" + "Name" + "-" + "X-MAN" + " KE-" + "COOL";
+				foreach (var kw in	engine.searchDistinct(box, searchText , long.MaxValue, 200)) {
+					Console.WriteLine (box ["TextObject", kw.getID ()].Select<TextObject> ());
+				}
+			}
+
+			Console.WriteLine ("");
+			//Full text search -or
+			using (var box = auto.Cube()) {
+				HashSet<long> ids = new HashSet<long> ();
+				Console.WriteLine ("Search: Time=2018-3-6 OR KVDesc={ \"Name\", \"X-MAN\" } , Keys={COOL}");
+			
+				String searchText = "TI-" + TextObject.DateTimeToStringShort (new DateTime (2018, 3, 6));
+				foreach (var kw in	engine.searchDistinct(box, searchText , long.MaxValue, 200)) {
+					ids.add (kw.getID ());
+				}
+
+				searchText = "KV-" + "Name" + "-" + "X-MAN" + " KE-" + "COOL";
+				foreach (var kw in	engine.searchDistinct(box, searchText , long.MaxValue, 200)) {
+					ids.add (kw.getID ());
+				}
+
+				long[] idslong = new long[ids.Count];
+				ids.CopyTo (idslong);
+				Array.Sort (idslong);
+
+				for (var i= idslong.Length-1; i>=0; i--) {
+					Console.WriteLine (box ["TextObject", idslong [i]].Select<TextObject> ());
+				}
+			}
+
+			auto.GetDatabase ().Dispose ();
+		}
+	}
+
+	public class TextObject
+	{ 
+		public string Content {
+			get;
+			set;
+		}
+
+		public int Group {
+			get;
+			set;
+		}
+
+		public Dictionary<string, object> KVDesc {
+			get;
+			set;
+		}
+
+		public string[] Keys {
+			get;
+			set;
+		}
+
+		public DateTime Time {
+			get;
+			set;
+		}
+
+		public int Season {
+			get;
+			set;
+		}
+
+		public long ID {
+			get;
+			set;
+		}
+
+		public override string ToString ()
+		{ 
+			StringBuilder sb = new StringBuilder ();
+			sb.append ("ID-" + ID + " ");
+			sb.append ("SE-" + Season + " ");
+			sb.append ("GR-" + Group + " ");
+			sb.append ("TI-" + DateTimeToStringShort (Time) + " ");
+			if (Keys != null) {
+				foreach (String str in Keys) {
+					sb.append ("KE-" + str + " ");
+				}
+			}
+			if (KVDesc != null) {
+				foreach (var kv in KVDesc) {
+					sb.append ("KV-" + kv.Key + "-" + kv.Value + " ");
+				}
+			}
+			sb.append (Content);
+			return sb.ToString ();
+		}
+
+		public static String DateTimeToStringShort (DateTime dt)
+		{
+			return dt.Year + "-" + dt.Month + "-" + dt.Day;
+		}
+	}
+}
